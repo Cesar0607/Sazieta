dbab20b2c5922a4642274578ffc701ae049de409
Cesar0607 <101206725+Cesar0607@users.noreply.github.com>
2022-05-20 21:02:12 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,87 @@
+const menu = document.querySelector('#mobile-menu');
+const linksMenu = document.querySelector('.links_menu');
+const logo = document.querySelector('#logo');
+
+// Menu Para celular
+const mobileMenu = () => {
+    menu.classList.toggle('esta-ativo');
+    linksMenu.classList.toggle('ativar');
+};
+
+menu.addEventListener('click', mobileMenu);
+
+// Fecha o Menu quando for clicado
+const fechaMenu = () => {
+    const barrinhaMenu = document.querySelector('.esta-ativo');
+    if (window.innerWidth <= 960 && barrinhaMenu) {
+        menu.classList.toggle('esta-ativo');
+        linksMenu.classList.remove('ativar');
+    };
+};
+
+linksMenu.addEventListener('click', fechaMenu);
+logo.addEventListener('click', fechaMenu);
+
+// Cardápio com JQuery
+$(document).ready(function () {
+
+    $('.controles .botoes').click(function () {
+        $(this).addClass('botao-ativo').siblings().removeClass('botao-ativo');
+
+        let filtro = $(this).attr('data-filter');
+        if (filtro == 'todos') {
+            $('.cardapio .imagem').show(400);
+        } else {
+            $('.cardapio .imagem').not('.' + filtro).hide(200);
+            $('.cardapio .imagem').filter('.' + filtro).show(200);
+        }
+    })
+});
+
+// Mostra no menu onde o usuário está na tela
+const marcadorMenu = () => {
+    const el = document.querySelector('.marcador');
+    const home = document.querySelector('#home-link');
+    const cardapio = document.querySelector('#cardapio-link');
+    let posicaoScroll = window.scrollY;
+
+    if (window.innerWidth > 960 && posicaoScroll < 600) {
+        home.classList.add('marcador');
+        cardapio.classList.remove('marcador');
+        equipe.classList.remove('marcador');
+        return;
+    } else if (window.innerWidth > 960 && posicaoScroll < 1400) {
+        cardapio.classList.add('marcador');
+        home.classList.remove('marcador');
+        equipe.classList.remove('marcador');
+        return;
+    } else if (window.innerWidth > 960 && posicaoScroll < 2345) {
+        equipe.classList.add('marcador');
+        cardapio.classList.remove('marcador');
+        home.classList.remove('marcador');
+        return;
+    };
+
+    if ((el && window.innerWidth < 960 && posicaoScroll < 600) || el) {
+        el.classList.remove('marcador');
+    }
+};
+
+window.addEventListener('scroll', marcadorMenu);
+window.addEventListener('click', marcadorMenu);
+
+
+// Swiper
+var swiper = new Swiper(".home_slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 7500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    loop: true,
+});
